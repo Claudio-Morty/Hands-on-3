ae43a1b3c40f802ee76d808ddc2118064129ef1b
Claudio-Morty <claudio.ponce1346@alumnos.udg.mx>
2024-10-09 06:10:11 +0000

    Update Create.sh

--- Create/Create.sh
+++ Create/Create.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+
+echo Se crea el archivo mytext con la cadena Hola Mundo
+
+echo "Hola Mundo" > mytext.txt
+
+echo Imprimo el contenido del archivo mytext
+
+cat mytext.txt
+
+echo Creo un directorio llamado Backup
+
+mkdir backup
+
+echo Moviendo el archivo mytext al directorio backup
+
+mv mytext.txt backup/
+
+echo Listo el contenido del directorio backup
+
+ls backup/
+
+echo Se elimina el archivo mytext del direcotrio baackup
+
+rm backup/mytext.txt
+
+echo Elimino el directorio backup
+
+rmdir backup
 
