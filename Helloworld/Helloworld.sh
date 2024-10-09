69b0028f67051f243f0e49f01fdc887f156c1fb7
Claudio-Morty <claudio.ponce1346@alumnos.udg.mx>
2024-10-09 06:09:03 +0000

    Update Helloworld.sh

--- Helloworld/Helloworld.sh
+++ Helloworld/Helloworld.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+# Imprimo en pantalla
+echo "Hello World"
+
+
+# Listo el contenido del directorio actual
+ls 
+
+
+#Creo el directorio
+mkdir Test
+
+
+#Cambio de directorio
+cd Test
+
+#Listo el directorio de Test
+ls
